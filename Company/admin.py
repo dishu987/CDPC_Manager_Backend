cd345ce898b5f2eaf84f07f1cd79babebfcedcb3
dishu001 <2020eeb1167@iitrpr.ac.in>
2023-06-22 09:19:22 +0530

    some changes

--- Company/admin.py
+++ Company/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Comment,CompanyTag,Company,HRDetails
 
 # Register your models here.
+admin.site.register(HRDetails)
+admin.site.register(CompanyTag)
+admin.site.register(Company)
+admin.site.register(Comment)
--- Company/models.py
+++ Company/models.py
@@ -3,24 +3,21 @@ from django.contrib.auth.models import User
 from users.models import UserModel
 
 
-class HRDetails(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=20)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='hr_details')
-
-
-class Comment(models.Model):
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='comments')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
 
 class CompanyTag(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
+
+class HRDetails(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+    
+
 
 class Company(models.Model):
     name = models.CharField(max_length=255)
@@ -37,4 +34,17 @@ class Company(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class Comment(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return "text: "+self.text+" /to: "+str(self.company)+" /by: "+str(self.user)
+    
+
+
     
